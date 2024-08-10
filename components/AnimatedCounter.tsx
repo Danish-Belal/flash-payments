313d5d7ff0bfbcc8dc120a28a42179ef7a091bd4
Danish-Belal <danishexplore019@gmail.com>
2024-08-10 16:12:43 +0530

    Animated Counter for amount count animation

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,15 @@
+'use client'
+import CountUp from 'react-countup'
+
+export const AnimatedCounter = ({amount}: {amount:number}) => {
+  return (
+    <div className='w-full'>
+          <CountUp 
+               decimals={2}
+               decimal=','
+               prefix='₹ '
+               end={amount} 
+          />
+    </div>
+  )
+}
