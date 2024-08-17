78adbb3c2edf13a846cfd600db6ef50a3f9aada7
Danish-Belal <danishexplore019@gmail.com>
2024-08-18 00:50:53 +0530

    FInilizing Dashboard

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 
           <div className="flex size-full flex-col">
             <div className="root-layout">
-              <Image  src="/icons/logo.svg" width={30} height={30} alt="Menu icon"/>
+              <Image  src="/icons/logo.svg" width={30} height={30} alt="logo"/>
             </div>
             <MobileNavBar user={loggedIn}/>
           </div>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 function Home() {
-     const loggedIn = {firstName: 'Danish'}
+     const loggedIn = {firstName: 'Danish' , lastName: 'Belal', email: 'danishexplore019@gmail.com'}
   return (
     <section className='home'>
      <div className='home-content'>
@@ -20,7 +21,13 @@ function Home() {
            totalBanks={2}
            totalCurrentBalance={12340.65}
           />
+          Recent Transacrions
      </div>
+     <RightSidebar 
+     user={loggedIn}
+     transactions={[ ]}
+     banks={[{currentBalance: 3323} , {currentBalance: 5473.09}]}
+     />
     </section>
   )
 }
