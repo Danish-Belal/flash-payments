21b752f42fbd74d7a75d14164d9d7174076fe282
Danish-Belal <danishexplore019@gmail.com>
2024-08-17 22:27:30 +0530

    SideBar Mobileview

--- components/MobileNavBar.tsx
+++ components/MobileNavBar.tsx
@@ -1,28 +1,99 @@
-import React from 'react'
+"use client";
+import React from "react";
 import {
-     Sheet,
-     SheetContent,
-     SheetDescription,
-     SheetHeader,
-     SheetTitle,
-     SheetTrigger,
-   } from "@/components/ui/sheet"
-import Image from 'next/image'
-   
-const MobileNavBar = ({user}: MobileNavProps) => {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+
+const MobileNavBar = ({ user }: MobileNavProps) => {
+  const pathname = usePathname();
   return (
-    <section className='w-full max-w-[264px]'>
-     <Sheet>
-  <SheetTrigger>
-     <Image src="/icons/hamburger.svg" width={30} height={30} alt='Menu' className='cursor-pointer'/>
-  </SheetTrigger> 
-  <SheetContent side={'left'} className='border-none bg-white'>
-    
-  </SheetContent>
-</Sheet>
-
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={30}
+            height={30}
+            alt="Menu"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-white">
+          <Link
+            href="/"
+            className="flex mb-12 cursor-pointer items-center gap-2"
+          >
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Flash Logo"
+            />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+              FlashPay
+            </h1>
+          </Link>
+          <div className="Mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathname === item.route ||
+                    pathname.startsWith(`${item.route}/`);
+                  return (
+                    <SheetClose asChild>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn("sidebar-link", {
+                          "bg-bank-gradient": isActive,
+                        })}
+                      >
+                        <div className="relative size-6">
+                          <Image
+                            src={item.imgURL}
+                            alt={item.label}
+                            width={20}
+                            height={20}
+                            className={cn({
+                              "brigtness-[3] invert-0": isActive,
+                            })}
+                          />
+                        </div>
+                        <p
+                          className={cn("text-16 font-semibold text-black-2  ", {
+                            "text-white": isActive,
+                          })}
+                        >
+                          {item.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+                USER
+              </nav>
+            </SheetClose>
+            Footer
+          </div>
+          
+        </SheetContent>
+      </Sheet>
     </section>
-  )
-}
+  );
+};
 
-export default MobileNavBar
+export default MobileNavBar;
+
+// sheet Component of shadcn
