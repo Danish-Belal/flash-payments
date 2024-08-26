c318be1a0e63fdb2ac6aefdcfe95065502ee6924
Danish-Belal <danishexplore019@gmail.com>
2024-08-26 19:50:41 +0530

    Transactions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import RecentTransactions from '@/components/RecentTransactions'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions'
@@ -6,16 +7,20 @@ import { getLoggedInUser } from '@/lib/actions/user.action'
 import React from 'react'
 
 const Home = async ({searchParams : {id,page}}:SearchParamProps ) => {
+     const currentPage = Number(page as string) || 1;
      const loggedInUser = await getLoggedInUser();
 
      const accounts = await getAccounts({userId: loggedInUser.$id});
      
      if(!accounts) return;
+
      const accountData = accounts?.data;
      const appwriteItemId = (id as string) || accountData[0]?.appwriteItemId;
-     console.log(appwriteItemId);
+     // console.log(appwriteItemId);
      
-     const acconts = await getAccount({appwriteItemId});
+     const account = await getAccount({appwriteItemId});
+     console.log("ACCOUNT" , account);
+     
 
      // console.log(
      //      accountData,
@@ -40,7 +45,13 @@ const Home = async ({searchParams : {id,page}}:SearchParamProps ) => {
           />
           </header>
 
-          Recent Transacrions
+          {/* Recent Transacrions */}
+          <RecentTransactions 
+               accounts= {accountData}
+               transactions = {accounts?.transactions}
+               appwriteItemId = {appwriteItemId}
+               page={currentPage}
+          />
      </div>
      
      <RightSidebar 
